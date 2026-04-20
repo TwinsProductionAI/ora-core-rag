4724ad1eade4945186660e4c99ffdd3729ce3f76
Xavier <xfleriag@gmail.com>
2026-04-20 15:03:10 -0400

    Export ARCH+ activation helpers

--- src/ora_core_rag/__init__.py
+++ src/ora_core_rag/__init__.py
@@ -1,5 +1,6 @@
-﻿"""ORA_CORE_RAG public Python package."""
+"""ORA_CORE_RAG public Python package."""
 
+from .arch_persona import ArchPersonaError, build_arch_persona_activation, load_activation_payload
 from .governor import GovernorConfig, GovernorError, RAGGovernor
 from .index import ORACoreIndex
 from .neroflux import NerofluxFanoutRegulator
@@ -7,6 +8,9 @@ from .registry import RAGRegistry, RegistryError
 from .route_gate import ClientRouteGate, RouteGateError
 
 __all__ = [
+    "ArchPersonaError",
+    "build_arch_persona_activation",
+    "load_activation_payload",
     "GovernorConfig",
     "GovernorError",
     "RAGGovernor",
